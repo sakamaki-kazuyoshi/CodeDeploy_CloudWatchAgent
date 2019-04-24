870a21e788582fbe8c53a2ea29302793a0ba984f
sakamaki-kazuyoshi <sakamaki.kazuyoshi@classmethod.jp>
2019-04-25 00:34:30 +0900

    firtst commit

---
+++ codedeploy/scripts/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+echo "Dokupepa"
+exit 0
---
+++ packer/scripts/cloudwatchagent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+cd /home/ec2-user
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
